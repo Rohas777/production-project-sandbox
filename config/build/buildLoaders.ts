d281b532bc3d4e55819cd5a6d1d54917f786c0cb
Rohas777 <olegpislyakov@mail.ru>
2025-09-08 07:44:56 +0300

    Initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,36 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            namedExport: false,
+            auto: (resPath: string) =>
+              Boolean(resPath.includes(".module.scss")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:8]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
+}
