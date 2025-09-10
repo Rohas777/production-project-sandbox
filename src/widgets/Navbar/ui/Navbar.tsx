e889e0c8febf54bc986f220b2eaa825eaf2290d6
Rohas777 <olegpislyakov@mail.ru>
2025-09-10 13:35:03 +0300

    add jest, add test for classNames helper

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import AppLink, { AppLinkTheme } from '@/shared/ui/AppLink/AppLink';
@@ -7,13 +8,16 @@ interface NavbarProps {
   className?: string;
 }
 
-const Navbar: FC<NavbarProps> = ({ className }) => (
-    <nav className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/">Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О нас</AppLink>
-        </div>
-    </nav>
-);
+const Navbar: FC<NavbarProps> = ({ className }) => {
+    const { t } = useTranslation();
+    return (
+        <nav className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О нас')}</AppLink>
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
